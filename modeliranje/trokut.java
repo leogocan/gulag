d9206eea1b8c948a1d8d88238fc94346277ad967
Leo Gočan <30301445+leogocan@users.noreply.github.com>
2017-07-23 16:55:58 +0200

    Update trokut.java

--- modeliranje/trokut.java
+++ modeliranje/trokut.java
@@ -4,6 +4,7 @@ public class trokut {
 	private double a;
 	private double b;
 	private double c;
+	//private double s; (procitaj dolje zasto mislim da treba tu ic)
 	
 	public trokut(double a, double b, double c) {
 		this.a=a;
@@ -30,10 +31,11 @@ public class trokut {
 		this.c = c;
 	}
 		
-	trokut trokut=new trokut(a, b, c);
+	trokut trokut=new trokut(a, b, c);  //brijem da ti je ovo nepotrebno tu, koliko znam ovo ide unutar main metode
 	
 	public double povrsina() {
-		double s= (this.a + this.b + this.c)/2;
+		double s= (this.a + this.b + this.c)/2;  /*mislim da ti ovo ne smije ic ovdje, vec gore ili mozda i moze al brijem da je ono gore 
+							    potrebno. Ja cu sad te kombe isprobavat pa vidjet radi li. */
 		return Math.sqrt(s*(s-this.a)*(s-this.b)*(s-this.c));
 		
 		
